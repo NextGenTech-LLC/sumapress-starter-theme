2efa0c55761707d5f771691fff11f4a907426278
sumapress <soportesumapress@gmail.com>
2018-12-08 12:10:47 +0100

    Add svg icon to responsive menu

--- header.php
+++ header.php
@@ -41,7 +41,10 @@
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sumapress-theme' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<?php //SVG from https://material.io/ or text: esc_html_e( 'Primary Menu', 'sumapress-theme' ); ?>
+					<img src="<?php echo get_template_directory_uri() . '/images/responsive-menu.svg'; ?>" alt="responsive-menu">
+				</button>
 				<?php
 					wp_nav_menu( array(
 						'theme_location' => 'menu-1',
